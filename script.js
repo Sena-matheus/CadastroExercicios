b7bb463016ededd512775c69996fdd701e5efed9
SenaaMatheus <matheussena2906@gmail.com>
2024-10-10 12:18:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js') // Ajuste o caminho conforme necessário
+            .then(registration => {
+                console.log('Service Worker registrado com sucesso:', registration);
+            })
+            .catch(error => {
+                console.error('Falha ao registrar o Service Worker:', error);
+            });
+    });
+}
+
 let exerciseActive = false;
 
 
